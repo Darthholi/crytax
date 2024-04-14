4478b3549293d52cbdd9477673e31c9210dce600
martin <martin.holecek.ai@gmail.com>
2024-04-14 20:52:03 +0200

    Rm typo

--- update_and_run.sh
+++ update_and_run.sh
@@ -2,4 +2,4 @@ git pull
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-python cry_download.py $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
+python cry_download.py $1 $2 $3 $4 $5 $6 $7 $8 $9
